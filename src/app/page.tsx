dcb36108cc19ad081f9b76ded7439188e1893787
Jordan Renaud <jordanrenaud@novata.com>
2024-07-22 17:23:08 +0100

    ad card styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { IconArrowNarrowLeft, IconArrowNarrowRight } from "@tabler/icons-react";
 import { useState } from "react";
 import { Button, Card, Link } from "../components/ui";
 const introductionText = [
@@ -12,19 +13,37 @@ export default function Home() {
   const [textIndex, setTextIndex] = useState(0);
   return (
     <div className="lg w-full flex flex-col justify-center items-center">
-      <Card>
-        <h1 className="block font-sans text-5xl font-semibold leading-tight tracking-normal text-inherit antialiased">
-          {introductionText[textIndex]}
-        </h1>
-      </Card>
-      <Button
-        text=""
-        onClick={() =>
-          textIndex >= introductionText.length - 1
-            ? console.log("going to first portfolio page")
-            : setTextIndex(textIndex + 1)
-        }
-      />
+      <div style={{ maxWidth: 500, height: 400 }}>
+        <Card
+          leftCta={
+            <Button
+              onClick={() =>
+                textIndex <= 0
+                  ? console.log("can't go further back")
+                  : setTextIndex(textIndex - 1)
+              }
+            >
+              <IconArrowNarrowLeft />
+            </Button>
+          }
+          rightCta={
+            <Button
+              onClick={() =>
+                textIndex >= introductionText.length - 1
+                  ? console.log("going to second portfolio page")
+                  : setTextIndex(textIndex + 1)
+              }
+            >
+              <IconArrowNarrowRight />
+            </Button>
+          }
+        >
+          <h1 className="block font-sans text-5xl font-semibold leading-tight tracking-normal text-inherit antialiased">
+            {introductionText[textIndex]}
+          </h1>
+        </Card>
+      </div>
+
       <Link href="/about" text="Oh come on, I've done this already" />
     </div>
   );
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,9 +1,40 @@
 import { ReactNode } from "react";
 
-export const Card = ({ children }: { children: ReactNode }) => {
+export const Card = ({
+  children,
+  leftCta,
+  rightCta,
+}: {
+  children: ReactNode;
+  leftCta: ReactNode;
+  rightCta: ReactNode;
+}) => {
   return (
-    <div className="w-96 flex flex-col justify-center align-center rounded-md border border-black p-11">
-      {children}
+    <div
+      style={{
+        height: "inherit",
+        color: "#033500",
+        border: "solid 1px #033500",
+        borderRadius: "1rem",
+        padding: "2rem",
+        display: "grid",
+        gridTemplateRows: "1fr 42px",
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateAreas: '"children children"\n"left right"',
+      }}
+    >
+      <div style={{ gridArea: "children" }}>{children}</div>
+
+      <div style={{ gridArea: "left" }}>{leftCta}</div>
+      <div
+        style={{
+          gridArea: "right",
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        {rightCta}
+      </div>
     </div>
   );
 };
